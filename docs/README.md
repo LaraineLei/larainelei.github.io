6ce452cb5cae45de753eeda497c7e14e6ffdb605
laraine <laraine_lei@163.com>
2019-03-08 19:00:18 +0800

    [config]

---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+actionText: Let's go
+actionLink: /localization/issue
+features:
+- title: 本地化平台
+  details: 用于管理公司翻译需求的流入、处理、反馈等多角色多态流程。
+- title: 权限平台
+  details: 用于管理公司后台系统的权限。
+footer: Thanks
+---
+<my-header> </my-header>
---
+++ node_modules/vuepress/lib/app/util.js
@@ -0,0 +1,33 @@
+export function pathToComponentName (path) {
+  if (path.charAt(path.length - 1) === '/') {
+    return `page${path.replace(/\//g, '-') + 'index'}`
+  } else {
+    return `page${path.replace(/\//g, '-').replace(/\.html$/, '')}`
+  }
+}
+
+export function findPageForPath (pages, path) {
+  for (let i = 0; i < pages.length; i++) {
+    const page = pages[i]
+    if (page.path === path) {
+      return page
+    }
+  }
+}
+
+export function getTitle (siteData, page) {
+  const selfTitle = page.frontmatter.home ? null : (
+    page.frontmatter.title || // explicit title
+    page.title // inferred title
+  )
+  const siteTitle = siteData.title
+  return siteTitle
+    ? selfTitle
+      ? (siteTitle + ' | ' + selfTitle)
+      : siteTitle
+    : selfTitle || 'VuePress'
+}
+
+export function getLang (page) {
+  return page.frontmatter.lang || 'en'
+}
